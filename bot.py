4c67342ce56ad8102c9bcc935775184bcbff7f8a
Barreloui <58188242+Barreloui@users.noreply.github.com>
2020-12-24 08:47:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,13 +36,10 @@ def printCoin(buy, sale):
     '''Вывод курса пользователю'''
     return "💰 *Курс покупки:* " + str(buy) + "\n💰 *Курс продажи:* " + str(sale)
 
-# Enable saving next step handlers to file "./.handlers-saves/step.save".
-# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
-# saving will hapen after delay 2 seconds.
+
 bot.enable_save_next_step_handlers(delay=2)
 
-# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
-# WARNING It will work only if enable_save_next_step_handlers was called!
+
 bot.load_next_step_handlers()
 
 if __name__ == '__main__':
